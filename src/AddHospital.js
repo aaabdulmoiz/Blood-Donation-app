4507ce94d8aaf0c9f034b069d839558e45dec672
aaabdulmoiz <amoizasif@khi.iba.edu.pk>
2020-04-18 03:06:46 +0500

    hospitallist

--- src/AddHospital.js
+++ src/AddHospital.js
@@ -1,14 +1,24 @@
-import React, { Component } from "react";
-import "./Apps.css";
-import ReactTable from "react-table";
-import "react-table/react-table.css";
+import React, { useState, useEffect } from "react";
 
-class AddHospital extends Component {
-  constructor(props) {
-    super(props);
+import axios from "axios";
 
-    this.state = {
-      hospitals: [],
+const AddHospital = () => {
+  const [post, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get("http://localhost:4000/hospitals");
+      setPosts(res.data);
+      setLoading(false);
     };
-  }
-}
+    fetchPosts();
+  }, []);
+  console.log(post);
+  return <div></div>;
+};
+
+export default AddHospital;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import SearchForRequest from "./SearchForRequest";
+//import SearchForRequest from "./SearchForRequest";
+import AddHospitals from "./AddHospital";
 
-ReactDOM.render(<SearchForRequest />, document.getElementById("root"));
+ReactDOM.render(<AddHospitals />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
