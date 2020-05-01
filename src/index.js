e2b3c6ea9af32a327bf1913d8f13e2e069a5f4c2
aaabdulmoiz <amoizasif@khi.iba.edu.pk>
2020-05-01 22:47:15 +0500

    bootsrap and jquery

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import AdminReq from "./AdminReq";
 //import SearchForRequest from "./SearchForRequest";
 //import AddHospitals from "./AddHospital";
-import Reqform from "./Reqform";
+//import Reqform from "./LandingPage";
 //import Search from "./Search";
 
-ReactDOM.render(<Reqform />, document.getElementById("root"));
+ReactDOM.render(<AdminReq />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
